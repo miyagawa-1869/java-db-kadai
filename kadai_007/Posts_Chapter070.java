97013e79db7cc93b7b4905ba8c5c50430805c8cf
miyagawa-1869 <answer.taba9@gmail.com>
2024-08-28 20:48:31 +0900

    Add files via upload
    kadai_070の修正

--- kadai_007/Posts_Chapter070.java
+++ kadai_007/Posts_Chapter070.java
@@ -11,11 +11,11 @@ import java.util.Date;
 public class Posts_Chapter070 {
 
 	public static void main(String[] args) {
-		//変数の宣言 createStatement メソッド		
+		//変数の宣言  メソッド		
 		Connection con = null;
 		Statement statement = null;
 
-		//データベース接続用のメソッド		
+		//データベース接続用のgetconnectionメソッド		
 		try {
 			con = DriverManager.getConnection(
 					"jdbc:mysql://localhost/challenge_java",
@@ -30,11 +30,11 @@ public class Posts_Chapter070 {
 
 			//追加するデータ用SQLを準備
 			String sql = "INSERT INTO posts(user_id,posted_at,post_content,likes) VALUES"
-					+ "('1003','2023-02-08','昨日の夜は徹夜でした・・','13'),"
-					+ "('1002','2023-02-08','お疲れ様です！','12'),"
-					+ "('1003','2023-02-09','今日も頑張ります！','18'),"
-					+ "('1001','2023-02-09','油断は禁物ですよ！','17'),"
-					+ "('1002','2023-02-10','明日から連休ですね！','20')";
+					+ "(1003,'2023-02-08','昨日の夜は徹夜でした・・',13),"
+					+ "(1002,'2023-02-08','お疲れ様です！',12),"
+					+ "(1003,'2023-02-09','今日も頑張ります！',18),"
+					+ "(1001,'2023-02-09','油断は禁物ですよ！',17),"
+					+ "(1002,'2023-02-10','明日から連休ですね！',20)";
 
 			//SQLクエリの実行（DBMSに送信する）	       
 			int rowCnt = statement.executeUpdate(sql);
@@ -65,14 +65,12 @@ public class Posts_Chapter070 {
 					statement.close();
 				} catch (SQLException ignore) {
 				}
-				;
 			}
 			if (con != null) {
 				try {
 					con.close();
 				} catch (SQLException ignore) {
 				}
-				;
 			}
 		}
 
